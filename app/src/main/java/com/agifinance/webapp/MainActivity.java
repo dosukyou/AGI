208270e7903433fd7adbf6099425d1687247de7f
dosukyou <dosukyou@users.noreply.github.com>
2024-12-30 13:23:57 -0500

    Update MainActivity.java

--- app/src/main/java/com/agifinance/webapp/MainActivity.java
+++ app/src/main/java/com/agifinance/webapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.agifinance.webapp;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +11,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WebView webView = new WebView(this);
         webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webView.getSettings().setJavaScriptEnabled(true);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setJavaScriptEnabled(true);
+
         webView.loadUrl("https://www.vizhole.com/report/");
         setContentView(webView);
     }
