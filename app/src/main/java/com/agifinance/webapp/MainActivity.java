6d76fb9486f2c2b9387f43110ee351f552bfb715
dosukyou <dosukyou@users.noreply.github.com>
2024-12-30 13:08:53 -0500

    Update MainActivity.java

--- app/src/main/java/com/agifinance/webapp/MainActivity.java
+++ app/src/main/java/com/agifinance/webapp/MainActivity.java
@@ -11,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView webView = new WebView(this);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(
+        	new WebViewClient() {
+            	@Override
+            	public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                	// Show a custom error message or retry
+                	view.loadData("<html><body><h3>Page not available</h3></body></html>", "text/html", "UTF-8");
+            	}
+            }
+        );
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.vizhole.com/report/");
         setContentView(webView);
